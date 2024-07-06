74a9d92d64e0ec50432ff454561cf68df4709f9b
boyapati nagendra <nagen4boyapati@gmail.com>
2024-07-06 22:25:28 +0530

    added additional aws resources

--- main.tf
+++ main.tf
@@ -2,28 +2,78 @@ provider "aws" {
   region = "us-west-2"
 }
 
-variable "cidr-blocks" {
-  description = "cidr blocks and name tags for vpc and subnet"
-  type        = list(object({
-    cidr_block = string
-    name = string
-  }))
-}
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "avail_zone" {}
+variable "env_prefix" {}
+variable "myip" {}
 
-resource "aws_vpc" "dev-vpc" {
-  cidr_block = var.cidr-blocks[0].cidr_block
+resource "aws_vpc" "myapp-vpc" {
+  cidr_block = var.vpc_cidr_block
 
   tags = {
-    Name = var.cidr-blocks[0].name
+    Name = "${var.env_prefix}-vpc"
   }
 }
 
-resource "aws_subnet" "dev-subnet-1" {
-  vpc_id            = aws_vpc.dev-vpc.id
-  cidr_block        = var.cidr-blocks[1].cidr_block
-  availability_zone = "us-west-2a"
+resource "aws_subnet" "myapp-subnet-1" {
+  vpc_id            = aws_vpc.myapp-vpc.id
+  cidr_block        = var.subnet_cidr_block
+  availability_zone = var.avail_zone
 
   tags = {
-    Name = var.cidr-blocks[1].name
+    Name = "${var.env_prefix}-subnet-1"
+  }
+}
+
+resource "aws_internet_gateway" "myapp-igw" {
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  tags = {
+    Name = "${var.env_prefix}-igw"
+  }
+}
+
+resource "aws_default_route_table" "main_rtb" {
+  default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.myapp-igw.id
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-main-rtb"
+  }
+}
+
+resource "aws_default_security_group" "default-sg" {
+
+  vpc_id = aws_vpc.myapp-vpc.id
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [var.myip]
+  }
+
+  ingress {
+    from_port = 8080
+    to_port = 8080
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"] 
+    prefix_list_ids = []   
+  }
+
+  tags = {
+    Name = "${var.env_prefix}-default-sg"
   }
 }
